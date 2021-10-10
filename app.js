78f1bda20f21b15d187f2e2fc9f25018687dfee5
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-10-09 20:05:37 -0600

    enter todo and user routes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+require("dotenv").config()
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var todoRouter = require('./routes/todo');
 
 var app = express();
 
@@ -15,7 +18,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +28,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
