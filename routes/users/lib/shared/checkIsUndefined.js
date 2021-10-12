6536387f2cf835b3cd4da937667d12fd0053ed46
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-10-11 21:03:10 -0600

    inital code entry

---
+++ routes/users/lib/shared/checkIsUndefined.js
@@ -0,0 +1,18 @@
+const undefined = '';
+
+function checkIsUndefined(req, res, next) {
+    if (Object.keys(req.body).length === 0 || req.body === undefined) {
+        return res
+            .status(500)
+            .json({
+                message: "Failure in checkIsUndefined",
+                error: "please fill out form"
+            })
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    checkIsUndefined
+};
